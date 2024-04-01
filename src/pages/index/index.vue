49e3be17c4501317f64b75b0b6a16eb54593d309
余小波 <yuxiaobo64@gmail.com>
2024-04-01 16:29:07 +0800

    fix 移除不必要的东西

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,10 @@
 <template>
     <view>
-        <navigator url="/pages/demo/index" open-type="navigate" hover-class="navigator-hover"> 效果演示 </navigator>
+        hello~
     </view>
 </template>
 
 <script setup lang="ts">
-import cache from "@/utils/cache";
-
-// cache.set('token', 111, 10)
-console.log(cache.get("token"));
 </script>
 
 <style lang="scss"></style>
