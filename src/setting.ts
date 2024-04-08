491695e398d61e03f58ecbc1cedf7995968bfd3d
余小波 <yuxiaobo64@gmail.com>
2024-04-08 22:25:52 +0800

    feat 应用名称和应用版本号获取

--- src/setting.ts
+++ src/setting.ts
@@ -1,3 +1,15 @@
+import manifest from "@/manifest.json";
+
+/**
+ * 应用名称
+ */
+export const appName = manifest.name;
+
+/**
+ * 应用版本
+ */
+export const appVersion = manifest.versionName;
+
 /**
  * 登录页面
  */
