92ba25988986d475b6311d87742f3a8efd7fd8d2
余小波 <yuxiaobo64@gmail.com>
2025-04-09 17:25:34 +0800

    feature 移除小程序通用分享

--- src/setting.ts
+++ src/setting.ts
@@ -19,13 +19,3 @@ export const loginPage = "/pages/login/login";
  * 白名单页面 （不做登录检查）
  */
 export const whiteList = [loginPage, "/"];
-
-/**
- * 小程序通用分享配置
- */
-export const shareConfig = {
-    // 默认分享标题
-    title: "分享标题",
-    // 默认分享图片 比例 5:4，可选，留空则是页面截图
-    imageUrl: ""
-};
